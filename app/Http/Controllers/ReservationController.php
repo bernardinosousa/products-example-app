5282eac035d6d01af73e7ab47ed7ae269f031ca8
Bernardino Sousa <bfgsousa@gmail.com>
2023-11-26 19:45:41 +0000

    Add initial ReservationController

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ReservationController extends Controller
+{
+    public function index()
+    {
+        //TODO
+    }
+
+    public function store(Request $request)
+    {
+        //TODO
+    }
+}
