ecfc83a8d25e7f2f8bae5d4153eb39453cfadba8
Bernardino Sousa <bfgsousa@gmail.com>
2023-11-27 01:16:17 +0000

    Updated products list page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,8 +7,22 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if(session()->has('success'))
+                <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+                    <span class="font-medium">{{ session()->get('success') }}</span>
+                </div>
+            @endif
+            @if ($errors->any())
+                @foreach ($errors->all() as $error)
+                <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                    <span class="font-medium">{{ $error }}</span>
+                </div>
+                @endforeach
+            @endif
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                
                 <div class="container mx-auto mx-auto p-4">
+                    
                     @if(count($products) == 0)
                         <div class="p-2 text-gray-900 dark:text-gray-100">
                             Currently no products are available in shop.
@@ -32,8 +46,10 @@
                                 </p>
                                 @endguest
                                 @auth
-                                <form class="py-4" method="POST" action="{{ route('reservations.store', ['product' => $product->id]) }}">
-                                    <input class=class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="number" placeholder="Quantity" value="1" min="1" max="{{ $product->quantity }}"/>
+                                <form class="py-4" method="POST" action="{{ route('reservations.store') }}">
+                                    @csrf
+                                    <input name="product_id" type="hidden" value="{{ $product->id }}"/> 
+                                    <input name="quantity" class=class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="number" placeholder="Quantity" value="1" min="1" max="{{ $product->quantity }}"/>
                                     <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="submit">Book</button>
                                 </form>
                                 @endauth
