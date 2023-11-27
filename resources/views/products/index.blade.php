88ecc163e7e585b2fe4ac7c8d258bbaa00fcd4ea
Bernardino Sousa <bfgsousa@gmail.com>
2023-11-27 01:38:24 +0000

    Add tabulation and product out of stock message

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -29,36 +29,47 @@
                         </div>
                     @else
                         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
-                        @foreach($products as $product)
-                        <div class="bg-white dark:bg-gray-800 rounded-lg p-4 border border-blue-200">
-                            <img src="https://placehold.co/300x200/d1d4ff/352cb5.png" alt="Placeholder Image" class="w-full h-48 rounded-md object-cover">
-                            <div class="px-1 py-4">
-                                <div class="font-bold text-xl mb-2 dark:text-gray-200">{{ $product->name }}</div>
-                                <p class="text-gray-700 dark:text-gray-200 text-base">
-                                    Price: {{ $product->price }} €
-                                </p>
-                                <p class="text-gray-700 dark:text-gray-200 text-sm">
-                                    Quantity: {{ $product->quantity }}
-                                </p>
-                                @guest
-                                <p class="text-gray-700 dark:text-gray-200 text-xs">
-                                    To see product price and make reservation you must be <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="{{ route('login') }}">logged in</a>
-                                </p>
-                                @endguest
-                                @auth
-                                <form class="py-4" method="POST" action="{{ route('reservations.store') }}">
-                                    @csrf
-                                    <input name="product_id" type="hidden" value="{{ $product->id }}"/> 
-                                    <input name="quantity" class=class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="number" placeholder="Quantity" value="1" min="1" max="{{ $product->quantity }}"/>
-                                    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="submit">Book</button>
-                                </form>
-                                @endauth
-                            </div>
-                            <div class="px-1 py-4">
-                            <a href="{{ route('products.show', ['product' => $product->id]) }}" class="text-blue-500 hover:underline">View More</a>
-                            </div>
-                        </div>
-                        @endforeach
+                            @foreach($products as $product)
+                                <div class="bg-white dark:bg-gray-800 rounded-lg p-4 border border-blue-200">
+                                    <img src="https://placehold.co/300x200/d1d4ff/352cb5.png" alt="Placeholder Image" class="w-full h-48 rounded-md object-cover">
+                                    <div class="px-1 py-4">
+                                        
+                                        <div class="font-bold text-xl mb-2 dark:text-gray-200">{{ $product->name }}</div>
+                                        
+                                        <p class="text-gray-700 dark:text-gray-200 text-base">
+                                            Price: {{ $product->price }} €
+                                        </p>
+
+                                        <p class="text-gray-700 dark:text-gray-200 text-sm">
+                                            Quantity: {{ $product->quantity }}
+                                        </p>
+
+                                        @guest
+                                            <p class="text-gray-700 dark:text-gray-200 text-xs">
+                                                To see product price and make reservation you must be <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="{{ route('login') }}">logged in</a>
+                                            </p>
+                                        @endguest
+
+                                        @auth
+
+                                            @if(!$product->quantity)
+                                                <p class="text-red-600">Out of stock</p>
+                                            @else
+                                                <form class="py-4" method="POST" action="{{ route('reservations.store') }}">
+                                                    @csrf
+                                                    <input name="product_id" type="hidden" value="{{ $product->id }}"/> 
+                                                    <input name="quantity" class=class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="number" placeholder="Quantity" value="1" min="1" max="{{ $product->quantity }}"/>
+                                                    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="submit">Book</button>
+                                                </form>
+                                            @endif
+                                        
+                                        @endauth
+                                    </div>
+                                    <div class="px-1 py-4">
+                                        <a href="{{ route('products.show', ['product' => $product->id]) }}" class="text-blue-500 hover:underline">View More</a>
+                                    </div>
+                                </div>
+                            @endforeach
                         </div>
                         {{ $products->links() }}
                     @endif
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -16,27 +16,34 @@
                         <img src="https://placehold.co/300x200/d1d4ff/352cb5.png" alt="Placeholder Image" class="w-full h-30 rounded-md object-cover">
                         <div class="px-1 py-4">
                             <div class="font-bold text-xl mb-2 dark:text-gray-200">{{ $product->name }}</div>
-                            <p class="py-2 text-gray-700 dark:text-gray-200 text-base">
+                                    
+                            <p class="text-gray-700 dark:text-gray-200 text-base">
                                 Price: {{ $product->price }} €
                             </p>
-                            <p class="py-2 text-gray-700 dark:text-gray-200 text-sm">
+
+                            <p class="text-gray-700 dark:text-gray-200 text-sm">
                                 Quantity: {{ $product->quantity }}
                             </p>
-                            <p class="py-2 text-gray-700 dark:text-gray-200 text-sm">
-                                Description: {{ $product->description }}
-                            </p>
+
                             @guest
-                            <p class="text-gray-700 dark:text-gray-200 text-xs">
-                                To see product price and make reservation you must be <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="{{ route('login') }}">logged in</a>
-                            </p>
+                                <p class="text-gray-700 dark:text-gray-200 text-xs">
+                                    To see product price and make reservation you must be <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="{{ route('login') }}">logged in</a>
+                                </p>
                             @endguest
+
                             @auth
-                            <form class="py-4" method="POST" action="{{ route('reservations.store') }}">
-                                @csrf
-                                <input name="product_id" type="hidden" value="{{ $product->id }}"/> 
-                                <input name="quantity" class=class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="number" placeholder="Quantity" value="1" min="1" max="{{ $product->quantity }}"/>
-                                <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="submit">Book</button>
-                            </form>
+
+                                @if(!$product->quantity)
+                                    <p class="text-red-600">Out of stock</p>
+                                @else
+                                    <form class="py-4" method="POST" action="{{ route('reservations.store') }}">
+                                        @csrf
+                                        <input name="product_id" type="hidden" value="{{ $product->id }}"/> 
+                                        <input name="quantity" class=class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="number" placeholder="Quantity" value="1" min="1" max="{{ $product->quantity }}"/>
+                                        <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="submit">Book</button>
+                                    </form>
+                                @endif
+                            
                             @endauth
                         </div>
                     </div>
