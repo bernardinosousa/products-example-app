7d31a6df7e52bfd1f513fae10aeeb082f1a4d2e6
Bernardino Sousa <bfgsousa@gmail.com>
2023-11-27 01:16:26 +0000

    Updated product page

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -31,8 +31,10 @@
                             </p>
                             @endguest
                             @auth
-                            <form class="py-4" method="POST" action="{{ route('reservations.store', ['product' => $product->id]) }}">
-                                <input class=class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="number" placeholder="Quantity" value="1" min="1" max="{{ $product->quantity }}"/>
+                            <form class="py-4" method="POST" action="{{ route('reservations.store') }}">
+                                @csrf
+                                <input name="product_id" type="hidden" value="{{ $product->id }}"/> 
+                                <input name="quantity" class=class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="number" placeholder="Quantity" value="1" min="1" max="{{ $product->quantity }}"/>
                                 <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="submit">Book</button>
                             </form>
                             @endauth
