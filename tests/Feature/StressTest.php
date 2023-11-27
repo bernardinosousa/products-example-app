e1abe76012853f3d3f0219995cba9d28bd7c39da
Bernardino Sousa <bfgsousa@gmail.com>
2023-11-27 10:50:48 +0000

    Add StressTest

---
+++ tests/Feature/StressTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use function Pest\Stressless\stress;
+
+test('has a average response time less than 100 ms', function () {
+    $result = stress('http://localhost')->for(10)->seconds();
+ 
+    expect($result->requests()->duration()->med())->toBeLessThan(100);
+});
