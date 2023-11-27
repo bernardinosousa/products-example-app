28363b5a79e3d4c800af2f53948f1e6a3dd6258b
Bernardino Sousa <bfgsousa@gmail.com>
2023-11-27 00:06:01 +0000

    Add products list pagination

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,12 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100 text-xl">
-                    {{ __("Products") }}
-                </div>
                 <div class="container mx-auto mx-auto p-4">
                     @if(count($products) == 0)
-                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <div class="p-2 text-gray-900 dark:text-gray-100">
                             Currently no products are available in shop.
                         </div>
                     @else
@@ -35,7 +32,6 @@
                                 </p>
                                 @endguest
                                 @auth
-                                
                                 <form class="py-4" method="POST" action="{{ route('reservations.store', ['product' => $product->id]) }}">
                                     <input class=class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="number" placeholder="Quantity" value="1" min="1" max="{{ $product->quantity }}"/>
                                     <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="submit">Book</button>
@@ -43,10 +39,11 @@
                                 @endauth
                             </div>
                             <div class="px-1 py-4">
-                            <a href="#" class="text-blue-500 hover:underline">Read More</a>
+                            <a href="{{ route('products.show', ['product' => $product->id]) }}" class="text-blue-500 hover:underline">View More</a>
                             </div>
                         </div>
                         @endforeach
+                        {{ $products->links() }}
                         </div>
                     @endif
                 </div>
